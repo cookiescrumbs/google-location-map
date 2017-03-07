b8ee34360643a149afcebe015b059d912ed245cd
Steven Cook <stevenjamescook@gmail.com>
2017-03-07 11:35:26 +0000

    some more stuff

---
+++ src/google-map.js
@@ -0,0 +1,41 @@
+import google from 'google';
+
+const render = () => {
+    const mapElement = document.querySelector('#location-map');
+    const mapOptions = () => ({
+        draggable: true,
+        streetViewControl: true,
+        mapTypeControlOptions: {
+            mapTypeIds: [
+                google.maps.MapTypeId.SATELLITE,
+                google.maps.MapTypeId.ROADMAP
+            ],
+        },
+        mapTypeId: google.maps.MapTypeId.SATELLITE
+    });
+
+    const map = new google.maps.Map(mapElement, mapOptions());
+    const lat = () => parseFloat(mapElement.dataset.lat) || null;
+    const lng = () => parseFloat(mapElement.dataset.lng) || null;
+    const zoom = () => parseInt(mapElement.dataset.zoom, 10) || 9;
+    const latLng = new google.maps.LatLng(lat(), lng());
+
+    const marker = () => {
+
+        const marker = new google.maps.Marker({ position: latLng, map: map });
+
+        return marker;
+    };
+
+    const center = {
+        lat: lat(),
+        lng: lng()
+    };
+
+    map.setCenter(center);
+    map.setZoom(zoom());
+
+    google.maps.event.addListenerOnce(map, 'idle', marker());
+};
+
+export { render };
--- src/index.js
+++ src/index.js
@@ -1,43 +1,5 @@
-import google from 'google';
+import * as googleMap from '../src/google-map.js';
 
-
-const render = () => {
-    const mapElement = document.querySelector('#location-map');
-    const mapOptions = () => ({
-        draggable: true,
-        streetViewControl: true,
-        mapTypeControlOptions: {
-            mapTypeIds: [
-                google.maps.MapTypeId.SATELLITE,
-                google.maps.MapTypeId.ROADMAP
-            ],
-        },
-        mapTypeId: google.maps.MapTypeId.SATELLITE
-    });
-
-    const map = new google.maps.Map(mapElement, mapOptions());
-    const lat = () => parseFloat(mapElement.dataset.lat) || null;
-    const lng = () => parseFloat(mapElement.dataset.lng) || null;
-    const zoom = () => parseInt(mapElement.dataset.zoom, 10) || 9;
-    const latLng = new google.maps.LatLng(lat(), lng());
-
-    const marker = () => {
-
-        const marker = new google.maps.Marker({ position: latLng, map: map });
-
-        return marker;
-    };
-
-    const center = {
-        lat: lat(),
-        lng: lng()
-    };
-
-    map.setCenter(center);
-    map.setZoom(zoom());
-
-    google.maps.event.addListenerOnce(map, 'idle', marker());
-};
-
-export { render };
-
+document.addEventListener('DOMContentLoaded', () => {
+    googleMap.render();
+});
